ce30130adebba16c4c3d08040f41930ceab54714
Cherry Poonolly <rpoonolly@gmail.com>
2019-03-19 20:21:11 +0700

    WIP - Nothing works anymore thanks to a chrome update

--- web_rtc_server/server.js
+++ web_rtc_server/server.js
@@ -5,17 +5,17 @@ console.log('Server Started! ...');
 io.on('connection', (socket) => {
     console.log('got a connection!');
 
-    socket.on('ice_candidate', (candidate) => {
+    socket.on('ice_candidate', async (candidate) => {
         console.log(`ice_candidate: ${candidate}`);
         socket.broadcast.emit('ice_candidate', candidate);
     });
 
-    socket.on('offer', (desc) => {
+    socket.on('offer', async (desc) => {
         console.log(`offer: ${desc}`);
         socket.broadcast.emit('offer', desc);
     });
 
-    socket.on('answer', (desc) => {
+    socket.on('answer', async (desc) => {
         console.log(`answer: ${desc}`);
         socket.broadcast.emit('answer', desc);
     });
