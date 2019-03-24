eeac57a843fc446d70d36ca7a82b335781f7f6d6
Cherry Poonolly <rpoonolly@gmail.com>
2019-03-25 06:31:10 +0700

    Added tabs for manual vs server mode

--- web_rtc_fe/src/App.js
+++ web_rtc_fe/src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 // material-ui
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
 import purple from '@material-ui/core/colors/purple';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 // web rtc stuff
 import WebRTCWithServerTab from './WebRTCWithServerTab/WebRTCWithServerTab';
@@ -17,14 +20,41 @@ const theme = createMuiTheme({
   },
 });
 
+const TAB_OPTIONS = Object.freeze({
+  SERVER: 'server',
+  MANUAL: 'manual',
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentTab: TAB_OPTIONS.SERVER
+    };
+
+    this.handleTabChange = this.handleTabChange.bind(this);
+  }
+
+  handleTabChange(event, value) {
+    this.setState({currentTab: value});
+  }
+
   render() {
+    const { currentTab } = this.state;
+
     return (
       <CssBaseline>
         <MuiThemeProvider theme={theme}>
           <div className="App">
-            <WebRTCWithServerTab></WebRTCWithServerTab>
+            <AppBar position="static">
+              <Tabs value={currentTab} onChange={this.handleTabChange}>
+                <Tab label="Connect by Server" value={TAB_OPTIONS.SERVER}/>
+                <Tab label="Connect Manually" value={TAB_OPTIONS.MANUAL}/>
+              </Tabs>
+            </AppBar>
+            {currentTab === TAB_OPTIONS.SERVER && <WebRTCWithServerTab></WebRTCWithServerTab>}
+            {currentTab === TAB_OPTIONS.MANUAL && <h1>Manual Mode!</h1>}
           </div>
         </MuiThemeProvider>
       </CssBaseline>
